12a5f5fd219c96646071e2db8f16a893eb132b1a
leijialiang <leijialiang@shanzhouweb.com>
2020-03-09 12:41:09 +0800

    feat: 代码自动滚动完成

--- miniprogram/components/code-type/code-type.js
+++ miniprogram/components/code-type/code-type.js
@@ -2,6 +2,11 @@ const computedBehavior = require('miniprogram-computed')
 const Prism = require('prismjs')
 const mockcode = require('../../mock/code')
 
+let scrollViewH = 0  // 滚动容器的高
+let lastcodeContentH = 0 // 代码区域上一次的高
+let codeContentH = 0 // 代码区域最新的高
+let s = null         // 元素选择器
+
 Component({
   behaviors: [computedBehavior],
 
@@ -10,7 +15,8 @@ Component({
     code: mockcode.code,
     currentCode: '',
     isCursorVisible: 1,
-    bottomEle: ''
+    // 代码容器需要滚动的高，用于实现一直滚动到底部
+    codeContentScrollTop: 0
   },
 
   computed: {
@@ -26,7 +32,13 @@ Component({
 
   lifetimes: {
     created: function() {
+      s = this.createSelectorQuery()
       this.progressivelyTyping()
+    },
+    ready: function() {
+      s.select('#scroll-view').boundingClientRect(function(rect){
+        scrollViewH = rect.height
+      }).exec()
     }
   },
 
@@ -43,6 +55,22 @@ Component({
           if(count % 2 === 0 && randomNumber % 4 === 0){
             this.setData({ currentCode: this.data.code.substring(0, typingCount) })
             typingCount++
+
+            s.select('#code-content').boundingClientRect(function(rect){
+              codeContentH = rect.height
+            }).exec()
+            
+            if (codeContentH > scrollViewH) {
+              if (codeContentH > lastcodeContentH) {
+                setTimeout(() => {
+                  this.setData({
+                    codeContentScrollTop: codeContentH - scrollViewH
+                  })
+                }, 0)
+                lastcodeContentH = codeContentH
+              }
+              
+            }
           }
 
           // 大约每 24 帧光标闪动一次
