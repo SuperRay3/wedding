64710ea9846046cccc4d3045fcd12a081c99b31d
leijialiang <leijialiang@shanzhouweb.com>
2020-03-22 23:53:30 +0800

    feat: 回复 cmd.js start 输出内容

--- miniprogram/components/terminal/cmd.js
+++ miniprogram/components/terminal/cmd.js
@@ -7,12 +7,7 @@ const cmds = {
       { label: '模块收集中...', type: 'default' },
       { label: '编译中...', type: 'default' },
       { label: '压缩中...', type: 'default' },
-      { label: '打包成功!', type: 'success' },
-      { label: '初始化', type: 'default' },
-      { label: '模块收集中...', type: 'default' },
-      { label: '编译中...', type: 'default' },
-      { label: '压缩中...', type: 'default' },
-      { label: '打包成功!', type: 'success' },
+      { label: '打包成功!', type: 'success' }
     ]
   },
   'reopen': {
