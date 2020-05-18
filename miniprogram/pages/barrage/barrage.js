56864f479035204f87c15e9f11cfc8dd3f7fa8a2
leijialiang <leijialiang@shanzhouweb.com>
2020-05-18 17:20:08 +0800

    feat: 轮询弹幕完成

--- miniprogram/pages/barrage/barrage.js
+++ miniprogram/pages/barrage/barrage.js
@@ -1,4 +1,12 @@
-let initBarrageData = []
+// 最新获取的弹幕，用于 addData 方法装填最新弹幕
+let stepLastestBarrage = []
+// 轮询弹幕配置
+let loopBarrageConfig = {
+  skip: 0,
+  limit: 20
+}
+// 轮询弹幕间隔
+let loopBarrageTick = 6000
 
 Page({
   data: {
@@ -6,8 +14,19 @@ Page({
   },
 
   async onReady() {
-    initBarrageData = await this.getBarrage()
-    this.initBarrageComp()
+    try {
+      stepLastestBarrage = await this.getBarrage(loopBarrageConfig)
+      loopBarrageConfig.skip += loopBarrageConfig.limit
+
+      this.initBarrageComp()
+    } catch(error) {
+      loopBarrageConfig.skip -= loopBarrageConfig.limit
+    }
+
+    // 开启轮询弹幕
+    // setTimeout(() => {
+    //   this.loopBarrageData()
+    // }, loopBarrageTick)
   },
 
   /**
@@ -21,13 +40,13 @@ Page({
       duration: 40,
       lineHeight: 2,
       mode: 'separate',
-      padding: [20, 20, 20, 20],
+      padding: [10, 0, 10, 0],
       maxLength: 50,
       safeGap: 4,
       tunnelShow: false,
     })
     this.barrage.open()
-    this.barrage.addData([initBarrageData])
+    this.barrage.addData([stepLastestBarrage])
   },
 
   /**
@@ -35,25 +54,50 @@ Page({
    * @param opts 条件配置
    */
 
-  async getBarrage(opts) {
+  getBarrage(opts) {
     const defaultParam = { skip: 0, limit: 100 }
     const fullParam = { ...defaultParam, ...opts }
-    const db = wx.cloud.database()
-    try {
-      const { data } = await db
-        .collection('barrage')
-        .skip(fullParam.skip)
-        .limit(fullParam.limit)
-        .get()
-      console.log(data)
-      return data
-    } catch (error) {
-      wx.showToast({
-        title: '祝福获取失败！',
-        duration: 1500,
+    return new Promise(async (res, rej) => {
+      const db = wx.cloud.database()
+      try {
+        const { data } = await db
+          .collection('barrage')
+          .skip(fullParam.skip)
+          .limit(fullParam.limit)
+          .get()
+        console.log(data)
+        res(data)
+      } catch (error) {
+        console.error('祝福获取失败！')
+        rej('error')
+      }
+    })
+    
+  },
+
+  /**
+   * 轮询弹幕数据
+   */
+
+  loopBarrageData() {
+    this.getBarrage(loopBarrageConfig)
+      .then(res => {
+        stepLastestBarrage = res
+
+        // 弹幕全部轮询完后，重置 skip 以达到循环播放的目的
+        if (res.length === 0) {
+          loopBarrageConfig.skip = 0
+        } else {
+          this.barrage.addData(stepLastestBarrage)
+          loopBarrageConfig.skip += loopBarrageConfig.limit
+        }
+
+        setTimeout(() => {
+          this.loopBarrageData()
+        }, loopBarrageTick)
+      }, rej => {
+        loopBarrageConfig.skip -= loopBarrageConfig.limit
       })
-      return []
-    }
   },
 
   /**
@@ -62,10 +106,7 @@ Page({
 
   onSendBarrage(e) {
     if (e.detail) {
-      setTimeout(() => {
-        this.barrage.addData([e.detail])
-      }, 4000)
+      this.barrage.addData([e.detail])
     }
-    console.log(e)
   },
 })
